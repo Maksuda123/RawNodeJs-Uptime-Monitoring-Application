f70a983bc59a714663872807af4ae2399450e71f
Maksuda Mila <48387926+Maksuda123@users.noreply.github.com>
2020-12-26 01:02:54 +0600

    RawNodeJs-Uptime-Monitoring-Application-
    Using Row Node JS

---
+++ routes.js
@@ -0,0 +1,18 @@
+/*
+*title:Hanndle Req Res
+*Description:
+*Author:
+*Date:
+*/
+const {sampleHandler} = require('./handlers/routesHandlers/sampleHandler');
+const {userHandler} = require('./handlers/routesHandlers/userHandler');
+const {tokenHandler} = require('./handlers/routesHandlers/tokenHandler');
+const {checkHandler} = require('./handlers/routesHandlers/checkHandler');
+const routes = {
+
+    sample: sampleHandler,
+    user: userHandler,
+    token: tokenHandler,
+    check: checkHandler,
+};
+module.exports = routes;
